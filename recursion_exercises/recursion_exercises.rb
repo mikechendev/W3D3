e231452c1ca650bab6078a8721bfee5aafe9c3a7
Vivian Chen <vnchen92@gmail.com>
2022-04-13 13:18:27 -0400

    Added more questions

--- recursion_exercises/recursion_exercises.rb
+++ recursion_exercises/recursion_exercises.rb
@@ -5,7 +5,7 @@ def range(start, finish)
     arr << arr[-1] + 1
 end
 
-p range(1,5)
+# p range(1,5)
 
 #def exp(b, n)
 #    return 1 if n == 0
@@ -24,6 +24,33 @@ def exp(b, n)
     end
 end
 
-p exp(2, 4)
-p exp(3, 3)
+# p exp(2, 4)
+# p exp(3, 3)
 
+class Array
+    def deep_dup
+        return self.dup if self.length == 1
+
+        duped = []
+        self.each do |subarray|
+            if subarray.is_a?(Array)
+                duped << subarray.deep_dup
+            else
+                duped << subarray
+            end
+        end
+        duped
+    end
+end
+
+# robot_parts = [
+#   ["nuts", "bolts", "washers"],
+#   ["capacitors", "resistors", "inductors"]
+# ]
+
+# robot_parts_copy = robot_parts.deep_dup
+
+# # shouldn't modify robot_parts
+# p robot_parts_copy[1] << "LEDs"
+# # but it does
+# p robot_parts[1] # => ["capacitors", "resistors", "inductors", "LEDs"]
